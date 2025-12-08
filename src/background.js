3b24aa8151db72190e2f496c8e51e828d7516d26
LochanaNK <lochanakariyawasam10@gmail.com>
2025-12-08 14:19:20 +0530

    ready to launch

--- src/background.js
+++ src/background.js
@@ -1,7 +1,7 @@
 chrome.runtime.onMessage.addListener((msg, sender, sendResponse) => {
   if (msg.action === "fetchPrice") {
-    // const backend = "https://price-extension.onrender.com";
-    const backend = "http://127.0.0.1:8080";
+    const backend = "https://price-extension.onrender.com";
+    // const backend = "http://127.0.0.1:8080";
 
 
     chrome.storage.local.set({ status: "loading", lastUrl: msg.productUrl });
